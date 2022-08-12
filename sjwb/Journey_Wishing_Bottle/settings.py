3cde68666fc19163cab66c27e663e12ae85cdfaa
w7147370218 <w7147370218@gamil.com>
2022-08-12 12:22:47 +0800

    make it better2

--- sjwb/Journey_Wishing_Bottle/settings.py
+++ sjwb/Journey_Wishing_Bottle/settings.py
@@ -122,5 +122,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
