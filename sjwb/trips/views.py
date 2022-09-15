88328665f8a1b449b437438b1030e20fc4296552
w7147370218 <w7147370218@gamil.com>
2022-09-15 14:39:32 +0800

    20220915 counties test4

--- sjwb/trips/views.py
+++ sjwb/trips/views.py
@@ -28,12 +28,13 @@ with open('County.zip', 'wb') as file:
 	file.write(response.content)
 	file.close()
 """
-
+import os
+dir = os.getcwd()
+counties_gdf = geopandas.read_file(dir+ '\\media\\'+'County.zip')
 
 def home(request):
 	posts = Post.objects.all()
 	
-	counties_gdf = geopandas.read_file('County.zip')
 	#map
 	m = folium.Map(location=[23.97565,120.9738819], zoom_start=8, height="100%", position="initial")
 
