d6d281aa1e022385b6d117bebbee577c9ff5b6a2
w71370218 <w7147370218@gmail.com>
2022-09-20 22:34:58 +0800

    test RWD

--- sjwb/trips/static/js/map.js
+++ sjwb/trips/static/js/map.js
@@ -4,8 +4,11 @@ window.onload = (event) => {
     let height = document.getElementsByClassName("navbar")[0].clientHeight
     let ele = document.getElementsByClassName("map")
     ele[0].children[0].children[0].style = ""
+    iframe = ele[0].children[0].children[0].children[1].contentWindow.document
     if (innerWidth < 1080) {
         ele[0].style = "margin-top:" + height + "px"
+        let layerfontsize = iframe.getElementsByClassName("leaflet-control-layers-overlays")[0]
+        layerfontsize.style = "font-size: 350%;"
     }
 
     //var layerControl = document.getElementsByClassName("leaflet-right")
